628c61df4fa7cd3b2b91e8fc244f22eabf2869f7
Simon Howroyd <howroydlsu@gmail.com>
2014-04-02 12:59:52 +0100

    copy and paste

--- loadbank.py
+++ loadbank.py
@@ -1,4 +1,4 @@
-4#!/usr/bin/python3
+4  #!/usr/bin/python3
 
 # TDi Loadbank Controller
 
@@ -23,92 +23,193 @@ import telnetlib, time
 
 
 class TdiLoadbank():
+    __LOAD_COMMAND = "load"
+    __MODE_COMMAND = "mode"
+    __VOLTAGE_COMMAND = "v"
+    __CURRENT_COMMAND = "i"
+    __POWER_COMMAND = "p"
+    __VOLTAGE_LIMIT_COMMAND = "vl"
+    __CURRENT_LIMIT_COMMAND = "il"
+    __POWER_LIMIT_COMMAND = "pl"
+    __VOLTAGE_MINIMUM_COMMAND = "uv"
+
+    __CONSTANT_VOLTAGE_COMMAND = "cv"
+    __CONSTANT_CURRENT_COMMAND = "ci"
+    __CONSTANT_POWER_COMMAND = "cp"
+
     def __init__(self, HOST, PORT=23, password=''):
-        self.HOST = HOST
-        self.PORT = PORT # Default 23 if not specified
-        self.password = password # Default blank if not specified
-        self.tn = self.__connect(HOST, PORT, password)
-        self.tn.read_very_eager() # Flush read buffer (needed)
+        self.__HOST = HOST
+        self.__PORT = PORT  # Default 23 if not specified
+        self.__password = password  # Default blank if not specified
+        self.__tn = self._connect(HOST, PORT, password)
 
     # Telnet connect method
-    @staticmethod
-    def __connect(HOST, PORT, password):
+    @classmethod
+    def _connect(cls, HOST, PORT, password):
         tn = telnetlib.Telnet(HOST, PORT)
         if password:
             tn.read_until(b"Password ? ")
             tn.write(password.encode('ascii') + b"\r\n")
+        cls._flush(tn)  # Flush read buffer (needed)
         return tn
 
+    @staticmethod
+    def _flush(tn):
+        tn.read_very_eager()  # Flush read buffer
+
+    @classmethod
+    def _set(cls, tn, command, value):
+        buf = (command + ' ' + value + '\r').encode('ascii')
+        cls._send(tn, buf)
+
+    @classmethod
+    def _get(cls, tn, command):
+        if not command.endswith('?'):
+            command += '?'
+        buf = (command + '\r').encode('ascii')
+        return cls._send(tn, buf)
+
     # Method to handle data 2way telnet datastream
-    @staticmethod
-    def __send(tn, command, value=''):
-        data = ""
-        tn.read_very_eager() # Flush read buffer (needed)
-        while not data or data.isspace():
-            # If we are setting a variable
-            if value:
-                tn.write((command + ' ' + value + '\r').encode('ascii'))
-#                print((command + ' ' + value + '\r').encode('ascii'))
-                time.sleep(0.1)
-                tn.read_very_eager() # Flush read buffer (needed)
-            # Request data
-            tn.write((command + '?\r').encode('ascii'))
-#            print((command + '?\r').encode('ascii'))
-            time.sleep(0.12)
-            # Receive data
-            data = tn.read_until(b"\r", 0.1) # Timeout = 0.1sec
-            data = data.decode('ascii')
-            data = data.strip('\r\n')
-            tn.read_very_eager() # Flush read buffer (needed)
+    @classmethod
+    def _send(cls, tn, inbuf):
+        cls._flush(tn)  # Flush read buffer (needed)
+        outbuf = ""
 
-        return data
+        while not outbuf or outbuf.isspace():
+            tn.write(inbuf)
+            if '?' in inbuf:
+                return
+            else:
+                time.sleep(0.12)  # Wait for reply, TODO not block
+                outbuf = tn.read_until(b'\r', 0.1)  # Timeout = 0.1sec TODO
+                outbuf = outbuf.strip('\r\n').decode('ascii')
+                cls._flush(tn)  # Flush read buffer (needed)
+        return outbuf
 
-    @staticmethod
-    def __get(request):
-        success = False
-        while success is False:
-            try:
-                result = request()
-                success = True
-            except (KeyboardInterrupt, SystemExit): raise
-            except: pass
-        return result
-        
+    # LOAD ON/OFF
+    @property
+    def load(self):
+        state = self._get(self.__tn, self.__LOAD_COMMAND)
+        if "on" in state:
+            return True
+        elif "off" in state:
+            return False
+        else:
+            return "UNKNOWN STATE"
 
-    # Random command
-    def random(self, command, state=''):
-        return self.__get(lambda: self.__send(self.tn, command, state))
+    @load.setter
+    def load(self, state):
+        if state:
+            self._set(self.__tn, self.__LOAD_COMMAND, "on")
+        else:
+            self._set(self.__tn, self.__LOAD_COMMAND, "off")
 
-    # Load
-    def load(self, state=''):
-        return self.__get(lambda: self.__send(self.tn, 'load', state).split(' ')[1])
 
-    # Mode
+    # MODE
+    @property
     def mode(self):
-        return self.__get(lambda: self.__send(self.tn, 'mode'))
+        my_mode = self._get(self.__tn, self.__MODE_COMMAND)
+        if "VOLTAGE" in my_mode:
+            return self._get(self.__tn, self.__CONSTANT_VOLTAGE_COMMAND)
+        elif "CURRENT" in my_mode:
+            return self._get(self.__tn, self.__CONSTANT_CURRENT_COMMAND)
+        elif "POWER" in my_mode:
+            return self._get(self.__tn, self.__CONSTANT_POWER_COMMAND)
+        else:
+            return "UNKNOWN MODE"
 
-    # Voltage
+    @mode.setter
+    def mode(self, op_mode):
+        op_mode = op_mode.lower()  # Change any capitals to lower case
+        if "vo" in op_mode or "cv" in op_mode:
+            self._set(self.__tn, self.__MODE_COMMAND, self.__CONSTANT_VOLTAGE_COMMAND)
+        elif "cu" in op_mode or "ci" in op_mode:
+            self._set(self.__tn, self.__MODE_COMMAND, self.__CONSTANT_CURRENT_COMMAND)
+        elif "po" in op_mode or "cp" in op_mode:
+            self._set(self.__tn, self.__MODE_COMMAND, self.__CONSTANT_POWER_COMMAND)
+
+
+    # VOLTAGE CONTROL
+    @property
     def voltage(self):
-        return self.__get(lambda: float(self.__send(self.tn, 'v').split(' ')[0]))
-    def constantVoltage(self, value=''):
-        return self.__get(lambda: float(self.__send(self.tn, 'cv', value).split(' ')[0]))
-    def voltageLimit(self, value=''):
-        return self.__get(lambda: float(self.__send(self.tn, 'vl', value).split(' ')[0]))
-    def voltageMinimum(self, value=''):
-        return self.__get(lambda: float(self.__send(self.tn, 'uv', value).split(' ')[0]))
+        return self._get(self.__tn, self.__VOLTAGE_COMMAND)
 
-    # Current
+    @voltage.setter
+    def voltage(self, volts):
+        self._set(self.__tn, self.__VOLTAGE_COMMAND, volts)
+
+    @property
+    def voltage_constant(self):
+        return self._get(self.__tn, self.__CONSTANT_VOLTAGE_COMMAND)
+
+    @voltage_constant.setter
+    def voltage_constant(self, volts):
+        self._set(self.__tn, self.__CONSTANT_VOLTAGE_COMMAND, volts)
+
+    @property
+    def voltage_limit(self):
+        return self._get(self.__tn, self.__VOLTAGE_LIMIT_COMMAND)
+
+    @voltage_limit.setter
+    def voltage_limit(self, volts):
+        self._set(self.__tn, self.__VOLTAGE_LIMIT_COMMAND, volts)
+
+    @property
+    def voltage_minimum(self):
+        return self._get(self.__tn, self.__VOLTAGE_MINIMUM_COMMAND)
+
+    @voltage_minimum.setter
+    def voltage_minimum(self, volts):
+        self._set(self.__tn, self.__VOLTAGE_MINIMUM_COMMAND, volts)
+
+
+    # CURRENT CONTROL
+    @property
     def current(self):
-        return self.__get(lambda: float(self.__send(self.tn, 'i').split(' ')[0]))
-    def constantCurrent(self, value=''):
-        return self.__get(lambda: float(self.__send(self.tn, 'ci', value).split(' ')[0]))
-    def currentLimit(self, value=''):
-        return self.__get(lambda: float(self.__send(self.tn, 'il', value).split(' ')[0]))
+        return self._get(self.__tn, self.__CURRENT_COMMAND)
 
-    # Power
+    @current.setter
+    def current(self, amps):
+        self._set(self.__tn, self.__CURRENT_COMMAND, amps)
+
+    @property
+    def current_constant(self):
+        return self._get(self.__tn, self.__CONSTANT_VOLTAGE_COMMAND)
+
+    @current_constant.setter
+    def current_constant(self, amps):
+        self._set(self.__tn, self.__CONSTANT_CURRENT_COMMAND, amps)
+
+    @property
+    def current_limit(self):
+        return self._get(self.__tn, self.__CURRENT_LIMIT_COMMAND)
+
+    @current_limit.setter
+    def current_limit(self, amps):
+        self._set(self.__tn, self.__CURRENT_LIMIT_COMMAND, amps)
+
+
+    # POWER CONTROL
+    @property
     def power(self):
-        return self.__get(lambda: float(self.__send(self.tn, 'p').split(' ')[0]))
-    def constantPower(self, value=''):
-        return self.__get(lambda: float(self.__send(self.tn, 'cp', value).split(' ')[0]))
-    def powerLimit(self, value=''):
-        return self.__get(lambda: float(self.__send(self.tn, 'pl', value).split(' ')[0]))
+        return self._get(self.__tn, self.__POWER_COMMAND)
+
+    @power.setter
+    def power(self, watts):
+        self._set(self.__tn, self.__POWER_COMMAND, watts)
+
+    @property
+    def power_constant(self):
+        return self._get(self.__tn, self.__CONSTANT_POWER_COMMAND)
+
+    @power_constant.setter
+    def power_constant(self, watts):
+        self._set(self.__tn, self.__CONSTANT_POWER_COMMAND, watts)
+
+    @property
+    def power_limit(self):
+        return self._get(self.__tn, self.__POWER_LIMIT_COMMAND)
+
+    @power_limit.setter
+    def power_limit(self, watts):
+        self._set(self.__tn, self.__POWER_LIMIT_COMMAND, watts)
